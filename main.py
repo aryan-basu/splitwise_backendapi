5f59e6eafd5eab9a0a80815936a0cd8725d7dbdb
aryan <aryanbasu005@gmail.com>
2024-11-24 23:53:44 +0530

    minor changes

--- main.py
+++ main.py
@@ -211,5 +211,5 @@ def search_user_by_email():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
 
