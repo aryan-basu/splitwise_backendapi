43c4fd5fb19808806a8ce8d0067053b41e683788
aryan <aryanbasu005@gmail.com>
2024-11-24 23:35:48 +0530

    minor port changes

--- main.py
+++ main.py
@@ -211,4 +211,5 @@ def search_user_by_email():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
